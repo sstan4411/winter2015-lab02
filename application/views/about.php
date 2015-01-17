a1587065da986b45bb72c256b1454c15a37167ce
sstan4411 <stefanstan92@gmail.com>
2015-01-16 19:25:09 -0800

    Added placeholders Signed-off-by:sstan4411 <stefanstan92@gmail.com>

--- application/views/about.php
+++ application/views/about.php
@@ -8,21 +8,7 @@
         <link rel="stylesheet" type="text/css" media="all" href="css/style.css" />
         <link rel="stylesheet" type="text/css" media="all" href="css/lightbox.css" />
     </head>
-    <body>
-        <div id="wrapper">
-            <div id="header">
-                <span class="myhead">Simple Image Gallery</span>
-                <span class="mynav">
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/Gallery.php">Gallery</a></li>
-                        <li><a href="/About.php">About</a></li>
-                    </ul>
-                </span>
-            </div>
-            <div class="alone"></div>
-            <div id="content">
-                <h1>About This Site</h1>
+    <h1>About This Site</h1>
                 <p>This is a really simple image gallery, intended as more than a "hello world" example'
                     to be implemented using different MVC frameworks, so they can be compared.</p>
                 <p>This site started out as the "Simple Gallery" from <a href="http://bryantsmith.com" target="_blank">Bryant Smith</a>.</p>
@@ -33,12 +19,3 @@
                 <p>The website uses the jquery Javascript framework, so that it can also use the jquery lightbox plugin.</p>
                 <h2>The Story</h2>
                 <p>This website was created for the Spring 2014 session of ACIT2910, at <a href="http://www.bcit.ca" target="_blank">BCIT</a>.</p>
-            </div>
-            <div id="footer" class="span12">
-                Copyright &copy; 2014,  <a href="mailto:someone@somewhere.com">Me</a>.
-            </div>
-        </div>
-        <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script> 
-        <script type="text/javascript" src="js/lightbox.min.js"></script> 
-    </body>
-</html>
--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>Simple Image Gallery</title>
-        <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
-        <link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/text.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/style.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/lightbox.css" />
-    </head>
-    <body>
-        <div id="wrapper">
-            <div id="header">
-                <span class="myhead">Simple Image Gallery</span>
-                <span class="mynav">
-                    <ul>
-                        <li><a href="/controllers/Welcome.php">Home</a></li>
-                        <li><a href="/controllers/Gallery.php">Gallery</a></li>
-                        <li><a href="/controllers/About.php">About</a></li>
-                    </ul>
-                </span>
-            </div>
-            <div class="alone"></div>
-            <div id="content">
-                <table cols="3" class="gallery">
+<table cols="3" class="gallery">
                     <tr>
                         <td class="oneimage">
                             <a href="./data/Katmai_Crater_1980.jpg" data-lightbox="gallery" data-title="Katmai Crater - Mount Katmai, Alaska ... Posted 2014.05.05 by donald, in landscape"><img src="./data/thumb/Katmai_Crater_1980.jpg"/></a>
@@ -37,12 +13,3 @@
                 </table>
                 <h1>This is just a simple image gallery :)</h1>
                 <p>The images above are the three most recently posted.</p>
-            </div>
-            <div id="footer" class="span12">
-                Copyright &copy; 2014,  <a href="mailto:someone@somewhere.com">Me</a>.
-            </div>
-        </div>
-        <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script> 
-        <script type="text/javascript" src="js/lightbox.min.js"></script> 
-    </body>
-</html>
